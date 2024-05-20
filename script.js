6e17a2e4cea728ca7c8a1b33fdb9dfa55a716a77
Rifuzada <84077010+Rifuzada@users.noreply.github.com>
2024-05-20 15:12:35 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,29 +1,46 @@
 document.addEventListener('DOMContentLoaded', function () {
   document.getElementById('search_button').addEventListener('click', function () {
-    // Obtém o valor do campo de entrada
     let puuid = ''
     let versao = ''
-
+    let region = document.querySelector('#region');
+    region = region.value;
+    region = region.toLowerCase()
+    var img = document.getElementById("iconImg")
+    var title = document.getElementById("title")
+    var container = document.getElementById("container")
     const riotId = document.getElementById('riotId').value;
-    nickName.innerHTML = riotId;
     axios.get('http://localhost:4000/account', { params: { riotId } })
       .then(response => {
         console.log('Dados recebidos da API de Account:', response.data);
         puuid = response.data.puuid;
-        console.log(puuid)
-
-        axios.get('http://localhost:4000/masteries', { params: { puuid } })
+        axios.get('http://localhost:4000/profile', { params: { puuid, region } })
+          .then(response => {
+            var iconID = response.data.profileIconId
+            var Level = response.data.summonerLevel
+            axios.get('http://ddragon.leagueoflegends.com/api/versions.json')
+              .then(response => {
+                versao = response.data[0]
+                img.src = `https://ddragon.leagueoflegends.com/cdn/${versao}/img/profileicon/${iconID}.png`;
+                img.style.display = 'block'
+                title.style.display = "none"
+                container.style.display = "block"
+                nickName.innerHTML = riotId + " -";
+                Lvl.innerHTML = "Level: " + Level;
+              });
+          });
+        axios.get('http://localhost:4000/masteries', { params: { puuid, region } })
           .then(maestrias => {
             var formatter = new Intl.NumberFormat("pt-BR");
             let arrayIDS = []
             let arrayNOMES = []
-
+            var maestriaData = Object.keys(maestrias.data)
+            let arrayLength = maestriaData.length
             axios.get('http://ddragon.leagueoflegends.com/api/versions.json')
               .then(response => {
                 versao = response.data[0]
                 axios.get(`https://ddragon.leagueoflegends.com/cdn/${versao}/data/en_US/champion.json`)
                   .then(nomes => {
-                    for (let i = 0; i < 5; i++) {
+                    for (let i = 0; i < arrayLength; i++) {
                       arrayIDS.push(maestrias.data[i].championId);
                     }
                     const allNames = Object.values(nomes.data.data)
@@ -43,13 +60,11 @@ document.addEventListener('DOMContentLoaded', function () {
 
 
           })
-          .catch(error => {
-            console.error('Erro ao chamar a API de Masteries:', error);
+          .catch(function (error) {
+            if (error.response) {
+              console.error('Erro ao chamar a API de Masteries:', error);
+            }
           });
       })
-      .catch(error => {
-        console.error('Erro ao chamar a API de Account:', error);
-      });
-
   });
 });
