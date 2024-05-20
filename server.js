f49ee5860a7f92907638bc979154313556a6fb24
Rifuzada <84077010+Rifuzada@users.noreply.github.com>
2024-05-20 15:12:06 -0300

    Update server.js

--- server.js
+++ server.js
@@ -6,17 +6,17 @@ var app = express();
 
 app.use(cors());
 
-const api_key = "RGAPI-04cc9072-f268-44ef-857a-1d52c5538957";
+const api_key = "";
 
 app.listen(4000, function () {
   console.log("Server started on port 4000")
 })
 
 const riotUrl = "https://americas.api.riotgames.com"
-const region = "br1"
-const riotUrlReg = `https://${region}.api.riotgames.com`
+let region = ""
 const endpointRiotId = "riot/account/v1/accounts/by-riot-id"
 const endpointPuuid = "lol/champion-mastery/v4/champion-masteries/by-puuid"
+const endpointSummonerPuuid = "lol/summoner/v4/summoners/by-puuid"
 
 app.get('/account', async (req, res) => {
   const { riotId } = req.query
@@ -26,15 +26,29 @@ app.get('/account', async (req, res) => {
     .then(response => response.data)
     .catch(err => err)
 
- res.json(response)
+  res.json(response)
 });
 
 app.get('/masteries', async (req, res) => {
   const { puuid } = req.query
+  const { region } = req.query;
+  const riotUrlReg = `https://${region}.api.riotgames.com`
   const puuidUrl = `${riotUrlReg}/${endpointPuuid}/${puuid}?api_key=${api_key}`
+  console.log(puuidUrl)
   const response = await axios.get(puuidUrl)
     .then(response => response.data)
     .catch(err => err)
-  console.log("chegou aqui")
   res.json(response)
 });
+
+app.get('/profile', async (req, res) => {
+  const { puuid } = req.query
+  const { region } = req.query;
+  const riotUrlReg = `https://${region}.api.riotgames.com`
+  const profileUrl = `${riotUrlReg}/${endpointSummonerPuuid}/${puuid}?api_key=${api_key}`
+  console.log(profileUrl)
+  const response = await axios.get(profileUrl)
+    .then(response => response.data)
+    .catch(err => err)
+  res.json(response)
+})
