b283d63b6049b32546bb14f27d89333f3ccf4f10
Rifuzada <84077010+Rifuzada@users.noreply.github.com>
2024-05-20 23:44:16 -0300

    Update server.js
    added ranked route

--- server.js
+++ server.js
@@ -13,10 +13,10 @@ app.listen(4000, function () {
 })
 
 const riotUrl = "https://americas.api.riotgames.com"
-let region = ""
 const endpointRiotId = "riot/account/v1/accounts/by-riot-id"
 const endpointPuuid = "lol/champion-mastery/v4/champion-masteries/by-puuid"
 const endpointSummonerPuuid = "lol/summoner/v4/summoners/by-puuid"
+const endpointRankedID = "lol/league/v4/entries/by-summoner"
 
 app.get('/account', async (req, res) => {
   const { riotId } = req.query
@@ -25,7 +25,6 @@ app.get('/account', async (req, res) => {
   const response = await axios.get(fullUrl)
     .then(response => response.data)
     .catch(err => err)
-
   res.json(response)
 });
 
@@ -34,7 +33,6 @@ app.get('/masteries', async (req, res) => {
   const { region } = req.query;
   const riotUrlReg = `https://${region}.api.riotgames.com`
   const puuidUrl = `${riotUrlReg}/${endpointPuuid}/${puuid}?api_key=${api_key}`
-  console.log(puuidUrl)
   const response = await axios.get(puuidUrl)
     .then(response => response.data)
     .catch(err => err)
@@ -46,8 +44,17 @@ app.get('/profile', async (req, res) => {
   const { region } = req.query;
   const riotUrlReg = `https://${region}.api.riotgames.com`
   const profileUrl = `${riotUrlReg}/${endpointSummonerPuuid}/${puuid}?api_key=${api_key}`
-  console.log(profileUrl)
   const response = await axios.get(profileUrl)
+    .then(response => response.data)
+    .catch(err => err)
+  res.json(response)
+})
+app.get('/ranked', async (req, res) => {
+  const { region } = req.query;
+  const { sumID } = req.query
+  const riotUrlReg = `https://${region}.api.riotgames.com`
+  const rankedUrl = `${riotUrlReg}/${endpointRankedID}/${sumID}?api_key=${api_key}`
+  const response = await axios.get(rankedUrl)
     .then(response => response.data)
     .catch(err => err)
   res.json(response)
