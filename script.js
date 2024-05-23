171d58bdd11da70ea7d5efef4022b46067c9410f
Rifuzada <84077010+Rifuzada@users.noreply.github.com>
2024-05-22 21:05:48 -0300

    added mastery search to the code
    now the mastery search works too

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 document.addEventListener("DOMContentLoaded", function () {
+  let containerMasteryI = []
   document
     .getElementById("search_button")
     .addEventListener("click", function () {
@@ -20,7 +21,9 @@ document.addEventListener("DOMContentLoaded", function () {
       var containerRankedFlex = document.getElementById("containerRankedFlex");
       var rankedFlexData = document.getElementById("rankedFlexData");
       var rankedFlexIcon = document.getElementById("rankedFlexIcon");
-      var containerMastery = document.getElementById("containerMasteries");
+      var iconDiv = document.getElementById("icon")
+      var containerContainer = document.getElementById("containerContainer")
+
 
       axios
         .get("http://localhost:4000/account", { params: { riotId } })
@@ -57,7 +60,6 @@ document.addEventListener("DOMContentLoaded", function () {
                     params: { sumID, region },
                   })
                   .then((ranked) => {
-                    console.log(ranked.data)
                     function noArena() {
                       if (
                         ranked.data[0]?.queueType == "RANKED_SOLO_5x5" ||
@@ -65,13 +67,13 @@ document.addEventListener("DOMContentLoaded", function () {
                         ranked.data[0]?.queueType == "RANKED_FLEX_SR" ||
                         ranked.data[1]?.queueType == "RANKED_FLEX_SR" ||
                         ranked.data[0] == undefined ||
-                        ranked.data1[1] == undefined
+                        ranked.data1[1] == undefined ||
+                        ranked.data == ""
                       ) {
                         if (
                           ranked.data[0] == undefined &&
-                          ranked.data[1].queueType == "RANKED_FLEX_SR"
+                          ranked.data[1]?.queueType == "RANKED_FLEX_SR"
                         ) {
-                          console.log("melaos");
                           rankedStats.innerHTML = "Unranked";
                           rankedSoloIcon.src = `https://raw.communitydragon.org/latest/plugins/rcp-fe-lol-static-assets/global/default/images/ranked-mini-crests/unranked.png`;
                           rankedSoloIcon.style.display = "flex";
@@ -92,9 +94,9 @@ document.addEventListener("DOMContentLoaded", function () {
                           rankedFlexData.innerHTML = `${queueFlex} ${ranked.data[1].tier} ${ranked.data[1].rank}\nVitorias: ${ranked.data[1].wins} <br>Derrotas: ${ranked.data[1].losses}`;
                         } else if (
                           ranked.data[1] == undefined &&
-                          ranked.data[0].queueType == "RANKED_FLEX_SR"
+                          ranked.data[0]?.queueType == "RANKED_FLEX_SR"
                         ) {
-                          console.log("1melaos");
+
                           rankedStats.innerHTML = "Unranked";
                           rankedSoloIcon.src = `https://raw.communitydragon.org/latest/plugins/rcp-fe-lol-static-assets/global/default/images/ranked-mini-crests/unranked.png`;
                           rankedSoloIcon.style.display = "flex";
@@ -113,9 +115,9 @@ document.addEventListener("DOMContentLoaded", function () {
                           rankedFlexData.innerHTML = `${queueFlex} ${ranked.data[0].tier} ${ranked.data[0].rank}\nVitorias: ${ranked.data[0].wins} <br>Derrotas: ${ranked.data[0].losses}`;
                         } else if (
                           ranked.data[1] == undefined &&
-                          ranked.data[0].queueType == "RANKED_SOLO_5x5"
+                          ranked.data[0]?.queueType == "RANKED_SOLO_5x5"
                         ) {
-                          console.log("13melaos");
+
                           rankedFlexIcon.src = `https://raw.communitydragon.org/latest/plugins/rcp-fe-lol-static-assets/global/default/images/ranked-mini-crests/unranked.png`;
                           rankedFlexIcon.style.display = "flex";
                           rankedFlexIcon.style.width = "40px";
@@ -134,9 +136,9 @@ document.addEventListener("DOMContentLoaded", function () {
                           rankedStats.innerHTML = `${queueSolo} ${ranked.data[0].tier} ${ranked.data[0].rank}\nVitorias: ${ranked.data[0].wins} <br>Derrotas: ${ranked.data[0].losses}`;
                         } else if (
                           ranked.data[0] == undefined &&
-                          ranked.data[1].queueType == "RANKED_SOLO_5x5"
+                          ranked.data[1]?.queueType == "RANKED_SOLO_5x5"
                         ) {
-                          console.log("1melaos3");
+
                           rankedFlexData.innerHTML = "Unranked";
                           rankedFlexIcon.src = `https://raw.communitydragon.org/latest/plugins/rcp-fe-lol-static-assets/global/default/images/ranked-mini-crests/unranked.png`;
                           rankedFlexIcon.style.display = "flex";
@@ -157,7 +159,7 @@ document.addEventListener("DOMContentLoaded", function () {
                           ranked.data[1] == undefined &&
                           ranked.data[0] == undefined
                         ) {
-                          console.log("2melaos");
+
                           rankedStats.innerHTML = "Unranked";
                           rankedSoloIcon.src = `https://raw.communitydragon.org/latest/plugins/rcp-fe-lol-static-assets/global/default/images/ranked-mini-crests/unranked.png`;
                           rankedSoloIcon.style.display = "flex";
@@ -170,11 +172,12 @@ document.addEventListener("DOMContentLoaded", function () {
                           rankedFlexIcon.style.width = "40px";
                           rankedFlexIcon.style.height = "40px";
                           containerRankedFlex.style.display = "block";
+                          rankedFlexData.innerHTML = "Unranked";
                         } else if (
-                          ranked.data[0].queueType == "RANKED_FLEX_SR" &&
-                          ranked.data[1].queueType == "RANKED_SOLO_5x5"
+                          ranked.data[0]?.queueType == "RANKED_FLEX_SR" &&
+                          ranked.data[1]?.queueType == "RANKED_SOLO_5x5"
                         ) {
-                          console.log("umitams33");
+
                           var queueFlex = ranked.data[0].queueType;
                           queueFlex = ranked.data[0].queueType
                             .replace("RANKED_FLEX_SR", "Ranqueada Flexiviel: ")
@@ -196,7 +199,7 @@ document.addEventListener("DOMContentLoaded", function () {
                           containerRanked.style.display = "block";
                           rankedStats.innerHTML = `${queueSolo} ${ranked.data[1].tier} ${ranked.data[1].rank}\nVitorias: ${ranked.data[1].wins} <br>Derrotas: ${ranked.data[1].losses}`;
                         } else if (ranked.data[0] == undefined) {
-                          console.log("mel3aos");
+
                           rankedStats.innerHTML = "Unranked";
                           rankedSoloIcon.src = `https://raw.communitydragon.org/latest/plugins/rcp-fe-lol-static-assets/global/default/images/ranked-mini-crests/unranked.png`;
                           rankedSoloIcon.style.display = "flex";
@@ -217,9 +220,9 @@ document.addEventListener("DOMContentLoaded", function () {
                           rankedFlexData.innerHTML = `${queueFlex} ${ranked.data[1].tier} ${ranked.data[1].rank}\nVitorias: ${ranked.data[1].wins} <br>Derrotas: ${ranked.data[1].losses}`;
                         } else if (
                           ranked.data[0] == undefined &&
-                          ranked.data[1].queueType == "RANKED_SOLO_5x5"
+                          ranked.data[1]?.queueType == "RANKED_SOLO_5x5"
                         ) {
-                          console.log("u1111mitams");
+
                           var queueSolo = ranked.data[1].queueType;
                           rankedStats.innerHTML = "Unranked";
                           rankedFlexIcon.src = `https://raw.communitydragon.org/latest/plugins/rcp-fe-lol-static-assets/global/default/images/ranked-mini-crests/unranked.png`;
@@ -236,10 +239,10 @@ document.addEventListener("DOMContentLoaded", function () {
                           containerRanked.style.display = "block";
                           rankedStats.innerHTML = `${queueSolo} ${ranked.data[1].tier} ${ranked.data[1].rank}\nVitorias: ${ranked.data[1].wins} <br>Derrotas: ${ranked.data[1].losses}`;
                         } else if (
-                          ranked.data[0].queueType == "RANKED_SOLO_5x5" &&
+                          ranked.data[0]?.queueType == "RANKED_SOLO_5x5" &&
                           ranked.data[1]?.queueType == "RANKED_FLEX_SR"
                         ) {
-                          console.log("aqui");
+
                           var queueSolo = ranked.data[0].queueType;
                           queueSolo = ranked.data[0].queueType
                             .replace("RANKED_FLEX_SR", "Ranqueada Flexiviel: ")
@@ -263,7 +266,7 @@ document.addEventListener("DOMContentLoaded", function () {
                           containerRankedFlex.style.display = "block";
                           rankedFlexData.innerHTML = `${queueFlex} ${ranked.data[1].tier} ${ranked.data[1].rank}\nVitorias: ${ranked.data[1].wins} <br>Derrotas: ${ranked.data[1].losses}`;
                         } else if (
-                          ranked.data[0].queueType == "RANKED_FLEX_SR"
+                          ranked.data[0]?.queueType == "RANKED_FLEX_SR"
                         ) {
                           var queueFlex = ranked.data[0].queueType;
                           rankedSoloIcon.style.display = "none";
@@ -295,13 +298,12 @@ document.addEventListener("DOMContentLoaded", function () {
                         }
                       }
                     }
-                    if (ranked.data[0].queueType == "CHERRY") {
-                      console.log("que");
+                    if (ranked.data[0]?.queueType == "CHERRY") {
                       delete ranked.data[0];
                       ranked.data[0] = undefined;
                       noArena()
                     } else if (ranked.data[1]?.queueType == "CHERRY") {
-                      console.log("qu111e");
+
                       delete ranked.data[1];
                       ranked.data[1] = undefined;
                       noArena()
@@ -341,30 +343,67 @@ document.addEventListener("DOMContentLoaded", function () {
                           arrayNOMES.push(championName);
                         }
                         let mastery1 = document.createElement("img");
-                        mastery1.src =
-                          "https://raw.communitydragon.org/latest/plugins/rcp-fe-lol-shared-components/global/default/mastery-9.png";
-                        containerMastery.appendChild(mastery1);
-                        containerMastery.style.display = ("block")
-                        console.log(
-                          `Champion: ${arrayNOMES[0]}, Possui ${formatter.format(maestrias.data[0].championPoints)} pontos de maestria` +
-                          `, com nível: ${maestrias.data[0].championLevel}`,
-                        );
-                        console.log(
-                          `Champion: ${arrayNOMES[1]}, Possui ${formatter.format(maestrias.data[1].championPoints)} pontos de maestria` +
-                          `, com nível: ${maestrias.data[1].championLevel}`,
-                        );
-                        console.log(
-                          `Champion: ${arrayNOMES[2]}, Possui ${formatter.format(maestrias.data[2].championPoints)} pontos de maestria` +
-                          `, com nível: ${maestrias.data[2].championLevel}`,
-                        );
-                        console.log(
-                          `Champion: ${arrayNOMES[3]}, Possui ${formatter.format(maestrias.data[3].championPoints)} pontos de maestria` +
-                          `, com nível: ${maestrias.data[3].championLevel}`,
-                        );
-                        console.log(
-                          `Champion: ${arrayNOMES[4]}, Possui ${formatter.format(maestrias.data[4].championPoints)} pontos de maestria` +
-                          `, com nível: ${maestrias.data[4].championLevel}`,
-                        );
+                        let mastery2 = document.createElement("img");
+                        let mastery3 = document.createElement("img");
+                        let mastery4 = document.createElement("img");
+                        let mastery5 = document.createElement("img");
+                        let containerMasteries1 = document.createElement("div");
+                        containerMasteries1.setAttribute("id", "containerMasteries1")
+                        let containerMasteries2 = document.createElement("div");
+                        containerMasteries2.setAttribute("id", "containerMasteries2")
+                        let containerMasteries3 = document.createElement("div");
+                        containerMasteries3.setAttribute("id", "containerMasteries3")
+                        let containerMasteries4 = document.createElement("div");
+                        containerMasteries4.setAttribute("id", "containerMasteries4")
+                        let containerMasteries5 = document.createElement("div");
+                        containerMasteries5.setAttribute("id", "containerMasteries5")
+                        let texto1 = document.createElement("span")
+                        let texto2 = document.createElement("span")
+                        let texto3 = document.createElement("span")
+                        let texto4 = document.createElement("span")
+                        let texto5 = document.createElement("span")
+
+                        function maestriaTela() {
+                          if (containerMasteryI.length == 0) {
+                            let maestriasI = [mastery1, mastery2, mastery3, mastery4, mastery5]
+                            containerMasteryI = [containerMasteries1, containerMasteries2, containerMasteries3, containerMasteries4, containerMasteries5]
+                            let textoI = [texto1, texto2, texto3, texto4, texto5]
+                            for (let i = 0; i < 5; i++) {
+
+                              if (maestrias.data[i].championLevel >= 10) {
+                                containerContainer.appendChild(containerMasteryI[i])
+                                textoI[i].innerHTML = `<br>Champion: ${arrayNOMES[i]}<br>${formatter.format(maestrias.data[i].championPoints)} pontos de maestria<br>` +
+                                  `nível: ${maestrias.data[i].championLevel}`
+                                maestriasI[i].src =
+                                  `https://raw.communitydragon.org/latest/plugins/rcp-fe-lol-shared-components/global/default/mastery-10.png`;
+                                containerMasteryI[i].appendChild(maestriasI[i]);
+                                containerMasteryI[i].appendChild(textoI[i]);
+                                containerMasteryI[i].style.display = ("block")
+                              }
+                              else if (maestrias.data[i].championLevel < 10) {
+                                containerContainer.appendChild(containerMasteryI[i])
+                                textoI[i].innerHTML = `<br>Champion: ${arrayNOMES[i]}<br>${formatter.format(maestrias.data[i].championPoints)} pontos de maestria<br>` +
+                                  `nível: ${maestrias.data[i].championLevel}`
+                                maestriasI[i].src =
+                                  `https://raw.communitydragon.org/latest/plugins/rcp-fe-lol-shared-components/global/default/mastery-${maestrias.data[i].championLevel}.png`;
+                                containerMasteryI[i].appendChild(maestriasI[i]);
+                                containerMasteryI[i].appendChild(textoI[i]);
+                                containerMasteryI[i].style.display = ("block")
+                              }
+                            }
+                          }
+                        }
+                        if (containerMasteryI.length == 0) {
+                          maestriaTela()
+                        }
+                        else if (containerMasteryI.length == 5) {
+                          for (let i = 0; i < 5; i++) {
+
+                            containerMasteryI[i].remove()
+                          }
+                          containerMasteryI = []
+                          maestriaTela()
+                        }
                       });
                   });
               })
@@ -376,6 +415,7 @@ document.addEventListener("DOMContentLoaded", function () {
           } else {
             title.style.display = "block";
             title.innerHTML = "Antes de clicar no botao escreva um Riot ID!";
+            title.style.color = ("")
           }
         });
     });
