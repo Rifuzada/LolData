1f4f76e7b41ce2c82f8802982447a6ba29ad14a4
Rifuzada <84077010+Rifuzada@users.noreply.github.com>
2024-05-24 09:59:47 -0300

    splited the vara part to allvars.js

--- script.js
+++ script.js
@@ -1,28 +1,10 @@
 import { allRequest, remove, sleep, show } from "./allRequest.js";
-let puuid = "";
-let versao = "";
-var region = document.querySelector("#region");
-var img = document.getElementById("iconImg");
-var title = document.getElementById("title");
-var container = document.getElementById("container");
-var btn = document.getElementById("search_button");
-var inputContainer = document.getElementById("input-container");
-var regionSelect = document.getElementById("region");
-var rankedSoloIcon = document.getElementById("rankedIcon");
-var containerRanked = document.getElementById("containerRanked");
-var rankedStats = document.getElementById("rankedSoloqData");
-var containerRankedFlex = document.getElementById("containerRankedFlex");
-var rankedFlexData = document.getElementById("rankedFlexData");
-var rankedFlexIcon = document.getElementById("rankedFlexIcon");
-var containerContainer = document.getElementById("containerContainer")
-let containerMasteryI = []
-var dotflashing = document.getElementById("dot-flashing");
-var preloaderBg = document.getElementById("preloaderBg");
+import { puuid, versao, region, img, title, container, btn, inputContainer, regionSelect, rankedSoloIcon, containerRanked, rankedStats, containerRankedFlex, rankedFlexData, rankedFlexIcon, containerContainer, dotflashing, preloaderBg } from "./allVars.js";
 
 document.addEventListener("DOMContentLoaded", function () {
   dotflashing.style.display = "none"
   preloaderBg.style.display = "none"
-  containerMasteryI = []
+  let containerMasteryI = []
   document
     .getElementById("search_button")
     .addEventListener("click", function () {
@@ -34,11 +16,11 @@ document.addEventListener("DOMContentLoaded", function () {
       }
       title.style.display = "none";
       dotflashing.style.display = "flex"
-      sleep(1000).then(() => {
+      sleep(800).then(() => {
         remove()
       })
-      allRequest(puuid, versao, region, img, title, container, btn, inputContainer, riotId, regionSelect, rankedSoloIcon, containerRanked, rankedStats, containerRankedFlex, rankedFlexData, rankedFlexIcon, containerContainer, containerMasteryI)
-      sleep(2800).then(() => {
+      allRequest(puuid, versao, region, img, container, btn, inputContainer, riotId, regionSelect, rankedSoloIcon, containerRanked, rankedStats, containerRankedFlex, rankedFlexData, rankedFlexIcon, containerContainer, containerMasteryI)
+      sleep(2000).then(() => {
         dotflashing.style.display = "none"
         show()
       })
