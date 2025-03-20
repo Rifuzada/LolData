e650e24b8902e003c16238c0d4a017603402cd74
Henrique Leme <henriqueleme.brito@outlook.com>
2025-03-19 22:41:43 -0300

    feat: routes + utils

---
+++ server/src/types/index.ts
@@ -0,0 +1,74 @@
+/**
+ * Interface para os dados da conta do jogador
+ */
+export interface Account {
+  puuid: string;
+  gameName: string;
+  tagLine: string;
+}
+
+/**
+ * Interface para os dados de perfil do jogador
+ */
+export interface Profile {
+  id: string;
+  accountId: string;
+  puuid: string;
+  name: string;
+  profileIconId: number;
+  revisionDate: number;
+  summonerLevel: number;
+}
+
+/**
+ * Interface para os dados de ranqueadas do jogador
+ */
+export interface RankedData {
+  leagueId: string;
+  queueType: string;
+  tier: string;
+  rank: string;
+  summonerId: string;
+  summonerName: string;
+  leaguePoints: number;
+  wins: number;
+  losses: number;
+  veteran: boolean;
+  inactive: boolean;
+  freshBlood: boolean;
+  hotStreak: boolean;
+}
+
+/**
+ * Interface para os dados de maestria de campeões
+ */
+export interface ChampionMastery {
+  championId: number;
+  championLevel: number;
+  championPoints: number;
+  lastPlayTime: number;
+  championPointsSinceLastLevel: number;
+  championPointsUntilNextLevel: number;
+  chestGranted: boolean;
+  tokensEarned: number;
+  summonerId: string;
+}
+
+/**
+ * Interface para os dados de um campeão
+ */
+export interface Champion {
+  id: string;
+  key: string;
+  name: string;
+  title: string;
+  image: {
+    full: string;
+    sprite: string;
+    group: string;
+    x: number;
+    y: number;
+    w: number;
+    h: number;
+  };
+}
