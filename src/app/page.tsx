995394c5ee0d6400a9d949c21405b3fad8d0914d
Rifu <rifuperez@gmail.com>
2025-03-23 13:12:54 -0300

    fixed matchhistory for others reigons and deleted unexistent regions that existed before

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -70,29 +70,29 @@ const Home: React.FC = () => {
           value={region}
           onChange={(e) => setRegion(e.target.value)}
         >
-          <option className='cursor-pointer' value="">🌎 Selecione uma região</option>
-          <optgroup label="Americas">
-            <option className='cursor-pointer' value="BR1">🇧🇷 - Brasil</option>
-            <option className='cursor-pointer'  value="NA1">🇺🇸 - América do Norte</option>
-            <option className='cursor-pointer'  value="LA1">🇲🇽 - América Latina Norte</option>
-            <option className='cursor-pointer'  value="LA2">🇦🇷 - América Latina Sul</option>
-          </optgroup>
-          <optgroup label="Europa">
-            <option className='cursor-pointer'  value="EUW1">🇪🇸 - Europa Oeste</option>
-            <option className='cursor-pointer'  value="EUN1">🇸🇪 - Europa Nórdica e Leste</option>
-            <option className='cursor-pointer'  value="RU">🇷🇺 - Rússia</option>
-            <option className='cursor-pointer'  value="TR1">🇹🇷 - Turquia</option>
-            <option className='cursor-pointer'  value="ME1">🇸🇦 - Arábia Saudita</option>
-          </optgroup>
-          <optgroup label="Ásia">
-            <option className='cursor-pointer'  value="KR">🇰🇷 - Coreia</option>
-            <option className='cursor-pointer'  value="JP1">🇯🇵 - Japão</option>
-            <option className='cursor-pointer'  value="TW2">🇹🇼 - Taiwan, Hong Kong e Macau</option>
-            <option className='cursor-pointer'  value="VN2">🇻🇳 - Vietnã</option>
-            <option className='cursor-pointer'  value="SG2">🇸🇬 - Singapura</option>
-          </optgroup>
-          <optgroup label="Oceania">
-            <option className='cursor-pointer'  value="OC1">🇦🇺 - Oceania</option>
+                <option className='cursor-pointer'  value="">🌎 Região</option>
+                <optgroup label="Americas">
+                <option className='cursor-pointer' value="BR1">🇧🇷 - Brasil</option>
+                <option className='cursor-pointer'  value="NA1">🇺🇸 - América do Norte</option>
+                <option className='cursor-pointer'  value="LA1">🇲🇽 - América Latina Norte</option>
+                <option className='cursor-pointer'  value="LA2">🇦🇷 - América Latina Sul</option>
+              </optgroup>
+              <optgroup label="Europa">
+                <option className='cursor-pointer'  value="EUW1">🇪🇸 - Europa Oeste</option>
+                <option className='cursor-pointer'  value="EUN1">🇸🇪 - Europa Nórdica e Leste</option>
+                <option className='cursor-pointer'  value="RU">🇷🇺 - Rússia</option>
+              </optgroup>
+              <optgroup label="Ásia">
+                <option className='cursor-pointer'  value="KR">🇰🇷 - Coreia</option>
+                <option className='cursor-pointer'  value="JP1">🇯🇵 - Japão</option>
+                <option className='cursor-pointer'  value="TW2">🇹🇼 - Taiwan, Hong Kong e Macau</option>
+                <option className='cursor-pointer'  value="TH2">🇹🇭 - Tailândia</option>
+                <option className='cursor-pointer'  value="VN2">🇻🇳 - Vietnã</option>
+                <option className='cursor-pointer'  value="TR1">🇹🇷 - Turquia</option>
+                <option className='cursor-pointer'  value="SG2">🇸🇬 - Singapura</option>
+              </optgroup>
+              <optgroup label="Oceania">
+                <option className='cursor-pointer'  value="OC1">🇦🇺 - Oceania</option>
           </optgroup>
         </select>
 
